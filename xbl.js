2fece0c5229d3a9de406aaa15ed562c6c4300c09
lukaschan <lorlno111@gmail.com>
2024-11-13 16:46:13 +0800

    Update xbl.js

--- xbl.js
+++ xbl.js
@@ -1,7 +1,7 @@
 /*
 小伴龙
 [rewrite_local]
-^https:\/\/xbl(?:pay|math|satellite)\.youban\.com url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/xbl.js
+^https:\/\/xbl(?:pay|math|satellite)\.youban\.com url script-response-body https://raw.githubusercontent.com/lukaschan/QuantumultX/refs/heads/main/xbl.js
 [mitm]
 
 hostname = *.youban.com
